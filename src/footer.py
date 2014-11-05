f19a30d41a1f9308f86f368c749ce397b6fdad59
Martin Nowak <martin.nowak@plugintheworld.com>
2014-11-05 20:30:21 +0100

    support multiple MongoDB instances
    - symlink files to e.g. /etc/munin/plugins/mongo_rs0_ops and mongo_rs1_ops

--- src/footer.py
+++ src/footer.py
@@ -11,6 +11,10 @@ if __name__ == "__main__":
     if 'PASSWORD' in environ:
         password = environ['PASSWORD']
     
+m = re.search("mongo(_.*)_(.*)$", sys.argv[0])
+if (m):
+    instance = m.group(1)
+
 if len(sys.argv) > 1 and sys.argv[1] == "config":
     doConfig()
 else:
